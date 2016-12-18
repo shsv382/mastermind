c5e86d83ddd7d42f5e717874ca510c46af8698b6
shsv382 <shsv382@gmail.com>
2016-12-18 21:54:13 +0400

    Mastermind с повторяющимися цифрами

--- mastermind.rb
+++ mastermind.rb
@@ -5,7 +5,7 @@ zagadannye = [rand(9)]
 
 while zagadannye.length < 4
 	inc = rand(9)
-	zagadannye << inc unless zagadannye.include?(inc)
+	zagadannye << inc #unless zagadannye.include?(inc)
 end
 
 #    ----- Начальные установки -----
@@ -26,15 +26,21 @@ while bulls < 4
 
 puts "#{zagadannye.join(' ')} \n #{vvod.join(' ')} \n"	# Вывод на экран загаданных и введенных цифр
 
-#  ----- Инкремент быков и коров -----
+#  ----- Инкремент быков -----
 
 	vvod.each_with_index do |cif, i| 
-		if zagadannye.include?(cif.to_i)
-			bulls += 1 if zagadannye[i] == cif.to_i
-		cows += 1
+		if zagadannye.include?(cif.to_i) && zagadannye[i] == cif.to_i
+			bulls += 1 
 		end
 	end
 
+#  ----- Инкремент коров -----
+
+	vvod.uniq.each do |cif|
+		cows += [vvod.count(cif), zagadannye.count(cif.to_i)].min
+	end
+
+#  ----- Убираем повторы -----
 	cows = cows - bulls
 	puts "#{bulls} быка, #{cows} коровы\n"
 end
